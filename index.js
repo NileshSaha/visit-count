c2fd410d45aa93ae82569a3cf092eabe140bb39d
NileshSaha <03.nileshsaha@gmail.com>
2024-05-18 14:01:24 +0530

    some comments added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const redis = require("redis");
+// const process = require("process");
 
 const app = express();
 const client  = redis.createClient({
@@ -10,6 +11,14 @@ const client  = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
+  // process.exit(0); // This will exit the container with status-code 0 i.e. everything is ok, but process wants to quit
+
+  /**
+   * if the status code is greater than 0 i.e. something went wrong so process wants to quit.
+   */
+
+
+
   client.get('visits', (err, visits) => {
     res.send('Number of visits are '+ visits);
     client.set('visits', parseInt(visits) + 1);
